6f7b98a1680447b0f95d2ff525a0000e742aa019
Mitchxxx <megboko@gmail.com>
2025-10-06 15:37:19 +0100

    Fixed lint errors

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -44,6 +44,4 @@ class AdminSiteTests(TestCase):
         """Test the create a user page"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-
         self.assertEqual(res.status_code, 200)
-
